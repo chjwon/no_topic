dfabd07d24f5c0fc63a40e0a6425a195617e9f5a
chjwon <chjwon10044@gmail.com>
2020-02-10 19:50:16 -0600

    //custom into 2Function

--- project_for_matrix/main.cpp
+++ project_for_matrix/main.cpp
@@ -9,13 +9,15 @@ using namespace std;
 
 const unsigned int MATRIX_SIZE = 20;
 
-void Function();
+void Function_txt();
+void Function_notxt();
 
 int main() {
-	Function();
+	Function_txt();
+	Function_notxt();
 }
 
-void Function() {
+void Function_txt() {
 
 
 	int row = 0, column = 0;
@@ -198,4 +200,149 @@ void Function() {
 		getch();
 	}
 
+}
+
+
+
+void Function_notxt() {
+	int row = 0, column = 0;
+	cin >> column >> row;
+	cout << endl;
+	//Input array
+	float** Input_arr = (float**)malloc(sizeof(float*) * column * row);
+
+
+	for (int i = 0; i < column; i++)
+		Input_arr[i] = (float*)malloc(sizeof(float) * row);
+
+	//Input the numbers
+	for (int i = 0; i < column; i++) {
+		for (int j = 0; j < row; j++) {
+			cin >> Input_arr[i][j];
+		}
+		cout << endl;
+	}
+
+
+	//sum array
+	float* Sum_arr = (float*)malloc(sizeof(float) * row);
+
+	//reset the sum array
+	for (int i = 0; i < row; i++) {
+		Sum_arr[i] = 0;
+	}
+
+	//get the sum array
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			Sum_arr[i] += Input_arr[j][i];
+		}
+	}
+
+	//print input
+	cout << "Input:" << endl;
+	cout << fixed;
+	cout.precision(3);
+	//cout << column << ' '<< row << endl;
+
+	for (int i = 0; i < column; i++) {
+		for (int j = 0; j < row; j++) {
+			cout << fixed;
+			cout.precision(3);
+			cout << ' ' << ' ' << ' ';
+			cout << Input_arr[i][j];
+		}
+		cout << endl;
+	}
+
+	//print sum
+
+	cout << "Sum:" << endl;
+	for (int i = 0; i < row; i++) {
+		cout << fixed;
+		cout.precision(3);
+		cout << ' ' << ' ' << ' ';
+		cout << Sum_arr[i];
+	}
+	cout << endl;
+
+	//vertical additive symmetry
+	cout << "Vertical additive symmetry : " << endl;
+
+
+	int middle = (row / 2);
+	int sym = 1;
+	for (int i = 0; i < middle; i++) {
+		if (Sum_arr[i] == Sum_arr[row - 1 - i]) {
+			continue;
+		}
+		else {
+			//cout << "No"<<endl;
+			//exit(0);
+			sym = 0;
+		}
+	}
+	if (sym == 1) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+	//cout << "Yes"<<endl;
+
+
+
+	//sort
+
+	int num_num = row * column;//size of 1D
+
+	//sort array
+	float* Sort_arr = (float*)malloc(sizeof(float) * num_num);
+
+
+
+	//ger the number into 1D array
+	for (int i = 0; i < column; i++) {
+		for (int j = 0; j < row; j++) {
+			int sort_num = i * row + j;
+			Sort_arr[sort_num] = Input_arr[i][j];
+		}
+	}
+
+	//need repair
+	//sort
+
+	//bubble sort
+	float temp;
+	for (int i = 0; i < num_num; i++) {
+		for (int j = 0; j < num_num - i - 1; j++) {
+			if (Sort_arr[j] > Sort_arr[j + 1]) {
+				temp = Sort_arr[j];
+				Sort_arr[j] = Sort_arr[j + 1];
+				Sort_arr[j + 1] = temp;
+			}
+		}
+	}
+
+
+	//make the Input array sorted
+
+	for (int i = 0; i < num_num; i++) {
+		int j;
+		j = i / row;
+
+		Input_arr[j][i % row] = Sort_arr[i];
+	}
+
+	//print sorted array
+	cout << "Sorted: " << endl;
+	for (int i = 0; i < column; i++) {
+		for (int j = 0; j < row; j++) {
+			cout << fixed;
+			cout.precision(3);
+			cout << ' ' << ' ' << ' ';
+			cout << Input_arr[i][j];
+		}
+		cout << endl;
+	}
 }
